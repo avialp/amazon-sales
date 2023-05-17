0bf622ad9498cae1b84d0666ef3d079dd77c3704
Avia <121842983+avialp@users.noreply.github.com>
2023-05-16 20:32:37 -0400

    dropped extra columns

--- amazon.sql
+++ amazon.sql
@@ -1,4 +1,4 @@
-SELECT * FROM amazon;
+SELECT * FROM amazon
 
 SELECT COUNT(product_id)
 FROM amazon;
@@ -6,5 +6,20 @@ FROM amazon;
 SELECT AVG(rating)
 FROM amazon;
 
+ALTER TABLE amazon 
+DROP COLUMN review_title
+
 SELECT actual_price, rating, product_name, product_id 
 FROM amazon;
+
+ALTER TABLE amazon 
+DROP COLUMN rating;
+
+SELECT COUNT(actual_price)
+FROM amazon;
+
+SELECT AVG(actual_price)
+FROM amazon;
+
+SELECT product_name, category, actual_price, discounted_price
+FROM amazon 
